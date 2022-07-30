6ffed0e7e13df785e3e943b2df61aa312be9d617
B. Sai Charan Reddy <saicharanreddyb17@gmail.com>
2022-07-30 14:37:33 +0000

    updated test bench

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -6,5 +6,41 @@ from cocotb.triggers import Timer
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
+    s = 0b01101
+    i = 0b01
 
-    cocotb.log.info('##### CTB: Develop your test here ########')
+    dut.sel.value = s
+    dut.inp13.value = i
+
+    await Timer(2, units='ns')
+
+    assert dut.out.value == 0b01, f"mux result is incorrect: {dut.X.value} != 0b01"
+        
+
+@cocotb.test()
+async def test_mux12(dut):
+    """Test for mux2"""
+    s = 0b11110
+    i = 0b10
+
+    dut.sel.value = s
+    dut.inp30.value = i
+
+    await Timer(2, units='ns')
+
+    assert dut.out.value == 0b10, f"mux result is incorrect: {dut.X.value} != 0b10"
+
+@cocotb.test()
+async def test_mux123(dut):
+    """Test for mux2"""
+    s = 0b11100
+    i = 0b100
+
+    dut.sel.value = s
+    dut.inp28.value = i
+
+    await Timer(2, units='ns')
+
+    assert dut.out.value == 0b100, f"mux result is incorrect: {dut.X.value} != 0b100"
+
+    
